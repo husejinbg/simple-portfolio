6ab133289230a9549ca2cde073f05548e4a9e2c0
husejinbg <husejinbg@gmail.com>
2024-06-26 17:13:34 +0300

    Created the home page template

--- main.py
+++ main.py
@@ -3,10 +3,33 @@ from fastapi.templating import Jinja2Templates
 import sqlite3
 import algolab
 import secretconfig
-client = algolab.API(secretconfig.MY_API_KEY, secretconfig.MY_USERNAME, secretconfig.MY_PASSWORD)
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+async def home(request: Request):
+    connection = sqlite3.connect("./app.db")
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+    assets = cursor.execute("""
+        SELECT
+            a.symbol,
+            a.name,
+            p.position,
+            p.average_price,
+            p.last_price,
+            p.position * p.last_price AS market_value
+        FROM portfolio p
+        JOIN assets a ON p.asset_id = a.id;
+    """).fetchall()
+
+    total = 0
+    for asset in assets:
+        total += asset["market_value"]
+
+    return templates.TemplateResponse("home.html", {"request": request, "assets": assets, "total": total})
+
 
 @app.get("/asset/{symbol}")
 async def asset_detail(request: Request, symbol: str):
