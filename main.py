f8a6739700ba7a1da6600d11af2365a0b5f133a3
husejinbg <husejinbg@gmail.com>
2024-06-27 10:52:07 +0300

    Total Market Value chart added to home page
    This chart shows the total market value of all the assets over time.

--- main.py
+++ main.py
@@ -3,9 +3,30 @@ from fastapi.templating import Jinja2Templates
 import sqlite3
 import algolab
 import secretconfig
+import pandas as pd
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+
+@app.get("/test")
+async def test(request: Request):
+    connection = sqlite3.connect("./app.db")
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+    data = cursor.execute("""
+        SELECT
+            timestamp,
+            SUM(position * last_price) AS market_value
+        FROM snapshots
+        GROUP BY timestamp
+    """).fetchall()
+    # df = pd.read_csv("./AAPL.csv")
+    # data = []
+    # for i in range(len(df)):
+    #     date = df.iloc[i]["Date"]
+    #     close = df.iloc[i]["Close"]
+    #     data.append({"date": date, "close": close})
+    return templates.TemplateResponse("test.html", {"request": request, "data": data})
 
 @app.get("/")
 async def home(request: Request):
@@ -28,10 +49,35 @@ async def home(request: Request):
     for asset in assets:
         total += asset["market_value"]
 
-    return templates.TemplateResponse("home.html", {"request": request, "assets": assets, "total": total})
+    data = cursor.execute("""
+        SELECT
+            timestamp,
+            SUM(position * last_price) AS market_value
+        FROM snapshots
+        GROUP BY timestamp
+    """).fetchall()
+
+    return templates.TemplateResponse("home.html", {"request": request, "assets": assets, "total": total, "data": data})
 
 
 @app.get("/asset/{symbol}")
 async def asset_detail(request: Request, symbol: str):
     asset = {"symbol": symbol}
-    return templates.TemplateResponse("asset_detail.html", {"request": request, "asset": asset})
+    connection = sqlite3.connect("./app.db")
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+    snapshots = cursor.execute("""
+        SELECT
+            position,
+            average_price,
+            last_price,
+            position * last_price AS market_value,
+            timestamp
+        FROM snapshots
+        WHERE asset_id = (
+            SELECT id FROM assets WHERE symbol = ?
+        )
+        ORDER BY id DESC
+    """, (symbol,)).fetchall()
+
+    return templates.TemplateResponse("asset_detail.html", {"request": request, "asset": asset, "snapshots": snapshots})
--- test.py
+++ test.py
@@ -1,3 +1,57 @@
-import db
+import sqlite3
+import yfinance
+import pandas as pd
+import config
 
-db.take_snapshot()
+symbols = ["HALKB.IS", "KCHOL.IS", "SKBNK.IS", "ZOREN.IS"]
+
+# for symbol in symbols:
+#     df = yfinance.download(symbol, start="2024-06-10")
+#     df.reset_index(inplace=True)
+#     df["Date"] = df["Date"].astype(str) + "T03:00:00+00:00"
+#     df.to_csv(f"./{symbol}.csv")
+
+def get_asset_id(symbol: str, name: str = None, exchange: str = None):
+    if name is None:
+        name = symbol
+    if exchange is None:
+        exchange = "BIST"
+
+    connection = sqlite3.connect(config.DB_FILE)
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+    row = cursor.execute("SELECT id FROM assets WHERE symbol = ?", (symbol,)).fetchone()
+    if row:
+        return row[0]
+    else:
+        cursor.execute("INSERT INTO assets (symbol, name, exchange) VALUES (?, ?, ?)", (symbol, name, exchange))
+        connection.commit()
+        return cursor.execute("SELECT id FROM assets WHERE symbol = ?", (symbol,)).fetchone()[0]
+
+def insert_snapshot(symbol, position, average_price, currency, last_price, timestamp):
+    connection = sqlite3.connect(config.DB_FILE)
+    cursor = connection.cursor()
+    asset_id = get_asset_id(symbol)
+
+    if position == -1:
+        position = cursor.execute("SELECT position FROM portfolio WHERE asset_id = ?", (asset_id,)).fetchone()[0]
+    
+    if average_price == -1:
+        average_price = cursor.execute("SELECT average_price FROM portfolio WHERE asset_id = ?", (asset_id,)).fetchone()[0]
+
+    cursor.execute("""
+        INSERT INTO snapshots (asset_id, position, average_price, currency, last_price, timestamp)
+        VALUES (?, ?, ?, ?, ?, ?)
+    """, (asset_id, position, average_price, currency, last_price, timestamp))
+    connection.commit()
+
+for symbol in symbols:
+    df = pd.read_csv(f"./{symbol}.csv")
+    symbol = symbol.replace(".IS", "")
+    for i in range(len(df)):
+        date = df.iloc[i]["Date"]
+        close = df.iloc[i]["Close"]
+        insert_snapshot(symbol, -1, -1, "TRY", close, date)
+        print(f"Inserted snapshot for {symbol} on {date}")
+    print(f"Inserted all snapshots for {symbol}")
+
